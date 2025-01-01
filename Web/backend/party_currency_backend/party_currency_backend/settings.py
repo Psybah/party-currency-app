332f02316d2b60c2268d359dba5bab3d7310fde3
Kayode Ojo <93586369+kayodeo1@users.noreply.github.com>
2024-12-31 23:24:11 -0800

    Configure CORS settings: restrict origins and allow specific methods for enhanced security

--- Web/backend/party_currency_backend/party_currency_backend/settings.py
+++ Web/backend/party_currency_backend/party_currency_backend/settings.py
@@ -37,6 +37,21 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1'
 ]
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    'https://party-currency-app-production.up.railway.app',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000'
+]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 # Database configuration
 DATABASES = {
