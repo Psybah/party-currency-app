660432568466a26fcc7a5d64e7c939e6821f36da
Kayode Ojo <93586369+kayodeo1@users.noreply.github.com>
2025-01-02 11:30:36 -0800

    Update CORS settings to allow all origins for local development

--- Web/backend/party_currency_backend/party_currency_backend/settings.py
+++ Web/backend/party_currency_backend/party_currency_backend/settings.py
@@ -41,7 +41,8 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     'https://party-currency-app-production.up.railway.app',
     'http://localhost:8000',
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000',
+    '*'
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
