c390d864b59b301d9d9a559ab2ff64ee77f5bedc
Psybah <abiodunoluwamurewa@gmail.com>
2025-01-04 18:07:05 +0100

    demo

--- Web/client/src/components/UserAvatar.jsx
+++ Web/client/src/components/UserAvatar.jsx
@@ -4,23 +4,22 @@ import { useNavigate, Link } from "react-router-dom";
 import { USER_PROFILE_CONTEXT } from "@/context";
 import { SIGNUP_CONTEXT } from "@/context";
 import { deleteAuth } from "@/lib/util";
-/**
- *
- * @param {boolean} showName show the name of the user
- * @param {boolean} auth give the user option to login or signup
- */
+
 export default function UserAvatar({ showName, auth }) {
   const { userProfile, setUserProfile } = useContext(USER_PROFILE_CONTEXT);
   const { setSignupOpen } = useContext(SIGNUP_CONTEXT);
   const navigate = useNavigate();
+
   const handleLogout = () => {
     setUserProfile(null);
     deleteAuth();
     navigate("/");
   };
+
   useEffect(() => {
     console.log("user Profile changed", userProfile);
   }, [userProfile]);
+
   let name = userProfile && userProfile.firstname;
 
   const options = (
@@ -28,15 +27,14 @@ export default function UserAvatar({ showName, auth }) {
       <ul className="space-y-2 mx-2 px-2 min-w-[10ch]">
         <li
           className="hover:font-semibold hover:text-Primary transition-colors cursor-pointer select-none"
-          onClick={() => {
-            handleLogout();
-          }}
+          onClick={handleLogout}
         >
           Sign out
         </li>
       </ul>
     </div>
   );
+
   return userProfile ? (
     <span className="select-none">
       <Popover
@@ -46,30 +44,15 @@ export default function UserAvatar({ showName, auth }) {
         mouseEnterDelay={0.3}
         mouseLeaveDelay={0.5}
       >
-        <div className="cursor-pointer">
-          {showName ? (
-            <div className="flex items-center bg-yellow-50 px-2 py-1.5 rounded-xl">
-              <Avatar
-                style={{
-                  backgroundColor: "#334495",
-                  verticalAlign: "middle",
-                }}
-                size="default"
-              >
-                <span className="font-semibold">{name[0]}</span>
-              </Avatar>
-              <span className="ml-2 font-semibold text-[#334495] algin-middle">
-                {name}
-              </span>
-            </div>
-          ) : (
-            <Avatar
-              style={{ backgroundColor: "#D6D979", verticalAlign: "middle" }}
-              size="default"
-            >
-              <span className="font-semibold">{name[0]}</span>
-            </Avatar>
-          )}
+        <div className="cursor-pointer flex items-center gap-2">
+          <span className="text-gray-600">Hello,</span>
+          <span className="font-semibold text-[#334495]">{name}</span>
+          <Avatar
+            style={{ backgroundColor: "#D6D979", verticalAlign: "middle" }}
+            size="default"
+          >
+            <span className="font-semibold">{name[0]}</span>
+          </Avatar>
         </div>
       </Popover>
     </span>
