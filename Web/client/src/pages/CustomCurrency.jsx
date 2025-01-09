dc84a75f2c7ab3c4fe04e9bc87dfc0510aa4180e
Psybah <abiodunoluwamurewa@gmail.com>
2025-01-09 18:03:30 +0100

    Custom currency page 20% complete

---
+++ Web/client/src/pages/CustomCurrency.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CustomCurrency = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-bluePrimary mb-6">Custom Currency</h1>
+      <p className="text-lg text-paragraph leading-relaxed">
+        Our custom currency system revolutionizes event spraying by allowing you
+        to create a personalized and secure cashless payment experience. With a
+        tap on your phone, manage transactions seamlessly and add a personal
+        touch to your celebrations.
+      </p>
+    </div>
+  );
+};
+
+export default CustomCurrency;
