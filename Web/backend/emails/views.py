8657d612c7a57dc7787c12c19470e57636bda69e
Kayode Ojo <93586369+kayodeo1@users.noreply.github.com>
2025-01-15 17:54:57 -0800

    Add password reset functionality with email verification and update event deletion message

---
+++ Web/backend/emails/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+# send_mail(subject= 'hi',
+#     message='test',
+#     from_email="",
+#     recipient_list=['ojokayode566@gmail.com'],
+# )
