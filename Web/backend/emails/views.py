f876713a9adbefd91023ff89858659a9177993b0
Kayode Ojo <93586369+kayodeo1@users.noreply.github.com>
2025-01-16 20:38:14 -0800

    Remove unused email app files and update authentication URLs for password management

--- Web/backend/emails/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.core.mail import send_mail
-
-# send_mail(subject= 'hi',
-#     message='test',
-#     from_email="",
-#     recipient_list=['ojokayode566@gmail.com'],
-# )
--- Web/backend/party_currency_backend/party_currency_backend/settings.py
+++ Web/backend/party_currency_backend/party_currency_backend/settings.py
@@ -81,12 +81,12 @@ DATABASES = {
 }
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 # Static files configuration for production
 STATIC_URL = '/static/'
