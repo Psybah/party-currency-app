6f21d05d7fa78c675678decfc2123f5e2a003f95
Kayode Ojo <93586369+kayodeo1@users.noreply.github.com>
2025-03-28 09:48:54 -0700

    Refactor URL patterns in events module to use hyphens instead of underscores for currency endpoints

--- Web/backend/party_currency_backend/events/urls.py
+++ Web/backend/party_currency_backend/events/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path("get/<str:id>", EventDetail, name="eventDetail"),
     path("update/<str:id>", EventUpdate, name="eventUpdate"),
     path("delete/<str:id>", EventDelete, name="eventDelete"),
-    path("save_currency", save_currency, name="save_currency"),
-    path("get_currency", get_currency, name="get_currency"),
+    path("save-currency", save_currency, name="save_currency"),
+    path("get-currency", get_currency, name="get_currency"),
 
 
     
