a7e6e080389ec763230687897aeecfdb683720e1
Kayode Ojo <93586369+kayodeo1@users.noreply.github.com>
2025-04-04 12:23:06 -0700

    fixed url issues

--- Web/backend/party_currency_backend/merchant/urls.py
+++ Web/backend/party_currency_backend/merchant/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import getAllTransaction,createReservedAccount,deleteReservedAccount
 urlpatterns = [
-    path("/transactions",getAllTransaction,name="list transactions"),
-    path("/create-reserved-account",createReservedAccount,name="create reserved account"),
-    path("/delete-reserved-account",deleteReservedAccount,name="delete reserved account")
+    path("transactions",getAllTransaction,name="list transactions"),
+    path("create-reserved-account",createReservedAccount,name="create reserved account"),
+    path("delete-reserved-account",deleteReservedAccount,name="delete reserved account")
     
 ]
